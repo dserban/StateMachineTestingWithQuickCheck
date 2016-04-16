707d4f7d146f659bfff702ee4ee0e5fd9d7e9bb1
Thomas66 <suthcr@yahoo.com>
2016-04-16 12:59:11 +0300

    threaddelay and func signature added

--- webdis/webdis_testing.hs
+++ webdis/webdis_testing.hs
@@ -1,4 +1,5 @@
-import Network.Curl (curlGetString)
+import Network.Curl ( curlGetString
+                    , CurlCode)
 
 import Test.QuickCheck ( Arbitrary
                        , Args
@@ -21,6 +22,8 @@ import Control.Applicative( (<$>)
                           , (<*>)
                           )
 
+import Control.Concurrent ( threadDelay )
+
 server = "http://127.0.0.1:7379/"
 
 data CustomSet = CustomSet { key :: String
@@ -31,9 +34,11 @@ get :: [Char] -> IO [Char]
 get key = do
         (a,b) <- curlGetString (server ++ "GET/" ++ key) []
         return $ drop 8 $ take (length b - 2) b
-        
+
+set :: CustomSet -> IO (CurlCode, String)
 set customSet = curlGetString (server ++ "SET/" ++ key customSet ++ "/" ++ value customSet) []
 
+flushall :: IO (CurlCode, String)
 flushall = curlGetString (server ++ "FLUSHALL") []
 
 instance Arbitrary CustomSet where
@@ -47,6 +52,7 @@ instance Arbitrary CustomSet where
 getsetHasExpectedBehavior :: CustomSet -> Property
 getsetHasExpectedBehavior customSet = monadicIO $ do
   _ <- run $ set customSet
+  _ <- run $ threadDelay 250000
   realityMatchesModel <- run $ ioStringsAreEqual first second
   assert realityMatchesModel
   where
