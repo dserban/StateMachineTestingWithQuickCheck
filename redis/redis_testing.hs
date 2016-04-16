d094d821a69871c29be11a12830049f7dc931691
Thomas66 <suthcr@yahoo.com>
2016-04-16 18:46:45 +0300

    redis_testing works

--- redis/redis_testing.hs
+++ redis/redis_testing.hs
@@ -1,8 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Database.Redis
-
-import Data.ByteString.Lazy.Char8
+import Database.Redis ( Connection
+                      , runRedis
+                      , get
+                      , set
+                      , flushall
+                      , connect
+                      , defaultConnectInfo
+                      )
 
 import Test.QuickCheck ( Arbitrary
                        , Args
@@ -27,32 +32,43 @@ import Control.Applicative( (<$>)
 
 import Control.Concurrent ( threadDelay )
 
-data CustomSet = CustomSet { key :: ByteString
-                           , value :: ByteString
+import Data.Maybe ( fromMaybe )
+
+import Data.ByteString.Char8 ( pack )
+
+data CustomSet = CustomSet { key :: String
+                           , value :: String
                            } deriving (Show)
 
 instance Arbitrary CustomSet where
   arbitrary = do
   key <- listOf1 genSafeChar
   value <- listOf1 genSafeChar
-  return ( CustomSet (pack key) (pack value) )
+  return ( CustomSet key value )
   where
     genSafeChar = elements $ ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9']
 
 getsetHasExpectedBehavior :: Connection -> CustomSet -> Property
 getsetHasExpectedBehavior conn customSet = monadicIO $ do
   _ <- run $ do
-    runRedis conn $ set (key customSet) (value customSet)
+    runRedis conn $ set (pack $ key customSet) (pack $ value customSet)
   realityMatchesModel <- run $ ioStringsAreEqual first second
   assert realityMatchesModel
   where
-    first = (return $ value customSet)
-    second = get $ key customSet
+    first = (return $ pack $ value customSet)
+    second = do
+      runRedis conn $ do
+        val <- get $ pack $ key customSet
+        case val of
+          Left _ -> return "Some error occurred"
+          Right v -> return $ fromMaybe "Could not find key in store" v
     ioStringsAreEqual s1 s2 = (==) <$> s1 <*> s2
 
 customArgs :: Args
 customArgs = ( stdArgs { maxSuccess = 1000000000 } )
 
+main :: IO ()
 main = do
   conn <- connect defaultConnectInfo
+  runRedis conn $ flushall
   quickCheckWith customArgs (getsetHasExpectedBehavior conn)
