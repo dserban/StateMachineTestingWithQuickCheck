4f427a41aeb91a8f113fe7ba8c01c0141866227e
Thomas66 <suthcr@yahoo.com>
2016-04-23 14:08:58 +0300

    sorted sets feature add

--- redis/redis_sortedsets.hs
+++ redis/redis_sortedsets.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Database.Redis ( Connection
+                      , RedisCtx
                       , runRedis
                       , get
                       , set
@@ -8,7 +9,9 @@ import Database.Redis ( Connection
                       , connect
                       , defaultConnectInfo
                       , zadd
+                      , zrangeWithscores
                       , zrem
+                      , zremrangebyrank
                       )
 
 import Test.QuickCheck ( Arbitrary
@@ -36,12 +39,49 @@ import Control.Concurrent ( threadDelay )
 
 import Data.Maybe ( fromMaybe )
 
+import Control.Monad.Trans ( liftIO )
+
 import Data.ByteString.Char8 ( ByteString
                              , pack
                              )
 
+data CustomSet = CustomSet { score :: Double
+                           , value :: ByteString
+                           } deriving (Show)
+
+instance Arbitrary CustomSet where
+ arbitrary = do
+ score <- arbitrary
+ value <- listOf1 genSafeChar
+ return ( CustomSet score (pack $ value) )
+ where
+   genSafeChar = elements $ ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9']
+
 sortedSetName = "sset"
 
-add score val = do
-  _ <- zadd sortedSetName [(score,val)]
-  return ()
+add :: RedisCtx m f => CustomSet -> m (f Integer)
+add customSet = zadd sortedSetName [(score customSet, value customSet)]
+
+customArgs :: Args
+customArgs = ( stdArgs { maxSuccess = 1000000000 } )
+
+sortedSetHasExpectedBehavior :: Connection -> CustomSet -> Property
+sortedSetHasExpectedBehavior conn customSet = monadicIO $ do
+  _ <- run $ do
+    threadDelay 500000
+    runRedis conn $ add customSet
+
+    getScore <- runRedis conn $ do
+      val <- zrangeWithscores sortedSetName 0 (-1)
+      case val of
+        Left _ -> return $ 0.0
+        Right v -> return $ snd $ head $ v
+
+    liftIO $ print getScore
+  assert $ 1 < 2
+
+main :: IO ()
+main = do
+  conn <- connect defaultConnectInfo
+  _ <- runRedis conn $ zremrangebyrank sortedSetName 0 (-1)
+  quickCheckWith customArgs (sortedSetHasExpectedBehavior conn)
